7d1f90429b326b4fac4949886abaae29da69079e
Double-Dev <deven.layton@gmail.com>
2025-04-18 16:00:45 -0600

    Added gui for login to DB.

--- dbms.py
+++ dbms.py
@@ -1,23 +1,42 @@
 import pg8000
+import requests as rq
 from getpass import getpass
 
-def create():
+connection = None
+
+def create(username: str, password: str):
     global connection
     global cursor
-    username = input("Please enter username: ")
     # Connect to DB
     connection = pg8000.connect(
         user=username,
-        password=getpass(),
+        password=password,
         host="ada.mines.edu",
         port=5432,
         database="csci403"
     )
     cursor = connection.cursor()
+    cursor.execute("SET search_path TO group8;")
 
-def select(table):
+def select(user_data):
     global cursor
-    cursor.execute("SELECT * FROM %s;", table)
+    print(cursor.execute("SELECT * FROM toronto_crimes LIMIT 1;"))
+    # address = user_data.address
+    # distance = user_data.distance
+    # date = user_data.date
+    # key = '&api_key=68001ed089ffb524504774xwb615f13'
+    # url = 'https://geocode.maps.co/search?q='
+    # lat_buff = distance/69
+    # long_buff = distance/42.4
+    # query = url+address+key
+    # res = rq.get(query)
+    # while (res.status_code == 401) or len(res.json()) < 1:
+    #     print("Invalid address. Try again.")
+    #     query = url+address+key
+    #     res = rq.get(query)
+    # lat = float(res.json()[0]['lat'])
+    # long = float(res.json()[0]['lon'])
+    # cursor.execute("SELECT * FROM toronto_crimes WHERE %s;", user_data)
 
 def insert():
     print("Not implemented yet.")
@@ -26,6 +45,7 @@ def destroy():
     global connection
     global cursor
     # Commit changes to DB
-    connection.commit()
-    cursor.close()
-    connection.close()
+    if connection != None:
+        connection.commit()
+        cursor.close()
+        connection.close()
--- main.py
+++ main.py
@@ -1,16 +1,45 @@
 import dearpygui.dearpygui as dpg
+import dbms
 
 dpg.create_context()
 dpg.create_viewport(title="Toronto Crime Statistics", width=800, height=400)
 dpg.setup_dearpygui()
 
-with dpg.window(label="Example Window"):
-    dpg.add_text("Hello, Dear PyGui")
+with dpg.value_registry():
+    dpg.add_string_value(tag="user_value")
+    dpg.add_string_value(tag="pass_value")
+
+def dataWindow():
+    with dpg.window(label="Data from query"):
+        dpg.add_text("temp test text")
+
+def login():
+    dbms.create(dpg.get_value("user_value"), dpg.get_value("pass_value"))
+    with dpg.window(tag="MainWindow"):
+        dpg.add_text("Hello, Dear PyGui")
+        # dpg.add_button(label="Test", callback=dataWindow)
+        # dpg.add_button(label="Test", callback=dbms.insert)
+        dpg.add_button(label="Connect", callback=dbms.create)
+        table = dpg.add_input_text(label="Table to query:")
+        dpg.add_button(label="Get", callback=dbms.select, user_data=table)
+    dpg.set_primary_window("MainWindow", True)
+    dpg.configure_item("LoginWindow", show=False)
+
+with dpg.window(tag="LoginWindow"):
+    dpg.add_text("Please Login:")
+    dpg.add_text("Username:")
+    dpg.add_same_line()
+    dpg.add_input_text(source="user_value")
+    dpg.add_text("Password:")
+    dpg.add_same_line()
+    dpg.add_input_text(source="pass_value", password=True)
+    dpg.add_button(label="Connect", callback=login)
+dpg.set_primary_window("LoginWindow", True)
 
 dpg.show_viewport()
-
 while dpg.is_dearpygui_running():
     dpg.render_dearpygui_frame()
 
 dpg.destroy_context()
+dbms.destroy()
 
