545b28356d5251292b82614803cedb0c1b860d4f
Double-Dev <deven.layton@gmail.com>
2025-04-21 15:18:19 -0600

    Cleaned up login warnings and UI.

--- main.py
+++ main.py
@@ -6,8 +6,11 @@ dpg.create_viewport(title="Toronto Crime Statistics", width=800, height=400)
 dpg.setup_dearpygui()
 
 with dpg.value_registry():
+    # Login Values
     dpg.add_string_value(tag="user_value")
     dpg.add_string_value(tag="pass_value")
+    # Temp Values
+    dpg.add_string_value(tag="temp_value")
 
 def dataWindow():
     with dpg.window(label="Data from query"):
@@ -15,24 +18,23 @@ def dataWindow():
 
 def login():
     dbms.create(dpg.get_value("user_value"), dpg.get_value("pass_value"))
+    dpg.delete_item("pass_value")
     with dpg.window(tag="MainWindow"):
-        dpg.add_text("Hello, Dear PyGui")
-        # dpg.add_button(label="Test", callback=dataWindow)
-        # dpg.add_button(label="Test", callback=dbms.insert)
-        dpg.add_button(label="Connect", callback=dbms.create)
-        table = dpg.add_input_text(label="Table to query:")
-        dpg.add_button(label="Get", callback=dbms.select, user_data=table)
+        with dpg.group(horizontal=True):
+            dpg.add_text("Table to query:")
+            dpg.add_input_text(source="temp_value")
+        dpg.add_button(label="Get", callback=dbms.select, user_data=dpg.get_value("temp_value"))
     dpg.set_primary_window("MainWindow", True)
-    dpg.configure_item("LoginWindow", show=False)
+    dpg.delete_item("LoginWindow")
 
 with dpg.window(tag="LoginWindow"):
     dpg.add_text("Please Login:")
-    dpg.add_text("Username:")
-    dpg.add_same_line()
-    dpg.add_input_text(source="user_value")
-    dpg.add_text("Password:")
-    dpg.add_same_line()
-    dpg.add_input_text(source="pass_value", password=True)
+    with dpg.group(horizontal=True):
+        dpg.add_text("Username:")
+        dpg.add_input_text(source="user_value")
+    with dpg.group(horizontal=True):
+        dpg.add_text("Password:")
+        dpg.add_input_text(source="pass_value", password=True)
     dpg.add_button(label="Connect", callback=login)
 dpg.set_primary_window("LoginWindow", True)
 
