c7084412b029c1d3f112800324fa29c3b8959384
Double-Dev <deven.layton@gmail.com>
2025-04-25 12:07:17 -0600

    Added insert functionality to dbms.py

--- dbms.py
+++ dbms.py
@@ -1,8 +1,11 @@
 import pg8000
 import requests as rq
-from getpass import getpass
+import math
+from datetime import date
+from datetime import datetime
 
 connection = None
+cursor = None
 
 def create(username: str, password: str):
     global connection
@@ -18,28 +21,81 @@ def create(username: str, password: str):
     cursor = connection.cursor()
     cursor.execute("SET search_path TO group8;")
 
-def select(user_data):
+def select(table):
     global cursor
-    print(cursor.execute("SELECT * FROM toronto_crimes LIMIT 1;"))
-    # address = user_data.address
-    # distance = user_data.distance
-    # date = user_data.date
-    # key = '&api_key=68001ed089ffb524504774xwb615f13'
-    # url = 'https://geocode.maps.co/search?q='
-    # lat_buff = distance/69
-    # long_buff = distance/42.4
-    # query = url+address+key
-    # res = rq.get(query)
-    # while (res.status_code == 401) or len(res.json()) < 1:
-    #     print("Invalid address. Try again.")
-    #     query = url+address+key
-    #     res = rq.get(query)
-    # lat = float(res.json()[0]['lat'])
-    # long = float(res.json()[0]['lon'])
-    # cursor.execute("SELECT * FROM toronto_crimes WHERE %s;", user_data)
+    # Cannot parameterize column or table names!
+    # cursor.execute("SELECT * FROM %s LIMIT 3;", (table,))
+    cursor.execute("SELECT * FROM "+table+" LIMIT 3;")
+    # cursor.execute("SELECT * FROM crimes_in_toronto LIMIT 3;")
+    results = cursor.fetchall()
+    return 0, results
 
-def insert():
-    print("Not implemented yet.")
+def crimesInArea(address, horiKm, vertKm):
+    key = '&api_key=68001ed089ffb524504774xwb615f13'
+    url = 'https://geocode.maps.co/search?q='
+    query = url+address+key
+    res = rq.get(query)
+    if (res.status_code == 401) or len(res.json()) < 1:
+        return 1, {}
+    lat = float(res.json()[0]['lat'])
+    long = float(res.json()[0]['lon'])
+    latDist = horiKm / 110.574
+    cursor.execute("""
+        SELECT lat_wgs84, long_wgs84 FROM toronto_crimes WHERE 
+            (lat_wgs84 >= %s AND lat_wgs84 <= %s) AND
+            (long_wgs84 >= %s AND long_wgs84 <= %s);
+        """, (lat-latDist, lat+latDist, long-vertKm/(111.32*math.cos(lat-latDist)), long+vertKm/(111.32*math.cos(lat+latDist))))
+    return 0, cursor.fetchall()
+
+def insert(ucr, offense, mciCategory, occuranceDate, occuranceHour, address, hoodCode, hoodName, premiseType, locationType):
+    global cursor
+    # Checking if occurance date is in DB and adding it if it's not:
+    # cursor.execute("SELECT * FROM occurance_date WHERE report_full_date = %s;", (occuranceDate))
+    # if cursor.fetchall().count() == 0:
+    cursor.execute("INSERT INTO occurance_date VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (occurance_full_date) DO NOTHING;;",
+                    (occuranceDate,
+                    occuranceDate.year,
+                    occuranceDate.month,
+                    occuranceDate.day,
+                    occuranceDate.timetuple().tm_yday,
+                    occuranceDate.weekday()))
+    
+    # Checking if report date is in DB and adding it if it's not:
+    reportDate = date.today()
+    # cursor.execute("SELECT * FROM occurance_date WHERE report_full_date = %s;", (reportDate))
+    # if cursor.fetchall().count() == 0:
+    cursor.execute("INSERT INTO report_date VALUES (%s, %s, %s, %s, %s, %s) ON CONFLICT (report_full_date) DO NOTHING;",
+            (reportDate,
+            reportDate.year,
+            reportDate.month,
+            reportDate.day,
+            reportDate.timetuple().tm_yday,
+            reportDate.weekday()))
+    
+    # Checking if location type is in DB and adding it if it's not:
+    cursor.execute("INSERT INTO location_type VALUES (%s, %s) ON CONFLICT (premis_type) DO NOTHING;",
+            (premiseType, locationType))
+    
+    # Checking if location is in DB and adding it if it's not:
+    key = '&api_key=68001ed089ffb524504774xwb615f13'
+    url = 'https://geocode.maps.co/search?q='
+    query = url+address+key
+    res = rq.get(query)
+    if (res.status_code == 401) or len(res.json()) < 1:
+        return 1, {}
+    lat = float(res.json()[0]['lat'])
+    long = float(res.json()[0]['lon'])
+    cursor.execute("INSERT INTO report_date VALUES (%s, %s, %s, %s, %s) ON CONFLICT (long_wgs84, lat_wgs84) DO NOTHING;",
+        (long, lat, premiseType, hoodCode, hoodName))
+
+    # Checking if crime_info is in DB and adding it if it's not:
+    cursor.execute("INSERT INTO crime_info VALUES (%s, %s, %s) ON CONFLICT (ucr) DO NOTHING;",
+        (ucr, offense, mciCategory))
+    
+    # Inserting crime into main crime table:
+    cursor.execute("INSERT INTO toronto_crimes VALUES (%s, %s, %s, %s, %s, NULL, %s, %s);",
+        (ucr, reportDate, occuranceDate, long, lat, datetime.now().hour, occuranceHour))
+
 
 def destroy():
     global connection
--- main.py
+++ main.py
@@ -5,12 +5,15 @@ dpg.create_context()
 dpg.create_viewport(title="Toronto Crime Statistics", width=800, height=400)
 dpg.setup_dearpygui()
 
-with dpg.value_registry():
-    # Login Values
-    dpg.add_string_value(tag="user_value")
-    dpg.add_string_value(tag="pass_value")
-    # Temp Values
-    dpg.add_string_value(tag="temp_value")
+def tempCallback():
+    dbms.select(dpg.get_value("table_value"))
+
+def areaSearchCallback():
+    status, result = dbms.crimesInArea(dpg.get_value("address_value"), dpg.get_value("hori_value"), dpg.get_value("vert_value"))
+    if status == 0:
+        with dpg.window(label="Crimes in Area"):
+            for row in result:
+                print(row)
 
 def dataWindow():
     with dpg.window(label="Data from query"):
@@ -20,10 +23,17 @@ def login():
     dbms.create(dpg.get_value("user_value"), dpg.get_value("pass_value"))
     dpg.delete_item("pass_value")
     with dpg.window(tag="MainWindow"):
+        # with dpg.group(horizontal=True):
+        #     dpg.add_text("Table to query:")
+        #     dpg.add_input_text(tag="table_value", default_value="crimes_in_toronto")
+        # dpg.add_button(label="Get", callback=tempCallback)
         with dpg.group(horizontal=True):
-            dpg.add_text("Table to query:")
-            dpg.add_input_text(source="temp_value")
-        dpg.add_button(label="Get", callback=dbms.select, user_data=dpg.get_value("temp_value"))
+            dpg.add_text("Address to Query:")
+            dpg.add_input_text(tag="address_value")
+        dpg.add_text("Region to Search in:")
+        dpg.add_input_float(tag="hori_value", min_value=0.1)
+        dpg.add_input_float(tag="vert_value", min_value=0.1)
+        dpg.add_button(label="Get", callback=areaSearchCallback)
     dpg.set_primary_window("MainWindow", True)
     dpg.delete_item("LoginWindow")
 
@@ -31,10 +41,10 @@ with dpg.window(tag="LoginWindow"):
     dpg.add_text("Please Login:")
     with dpg.group(horizontal=True):
         dpg.add_text("Username:")
-        dpg.add_input_text(source="user_value")
+        dpg.add_input_text(tag="user_value", default_value="dlayton")
     with dpg.group(horizontal=True):
         dpg.add_text("Password:")
-        dpg.add_input_text(source="pass_value", password=True)
+        dpg.add_input_text(tag="pass_value", password=True)
     dpg.add_button(label="Connect", callback=login)
 dpg.set_primary_window("LoginWindow", True)
 
